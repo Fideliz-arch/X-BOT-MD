15f517e78185e90cd04d9a695456bb5ab21678f4
Fideliz-arch <pheazy31@gmail.com>
2025-07-19 01:34:15 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "XBOT-MD**AOOJNXMZOD3LPOW",
+SESSION_ID: process.env.SESSION_ID || "XBOT-MD**3XAM96NRYQRZEYE",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
